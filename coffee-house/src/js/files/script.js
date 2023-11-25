886d771be72e5922582ffd0374a83888a36e722e
Aleksandr Shumatbaev <sh889@ya.ru>
2023-11-25 22:17:56 +0500

    feat: add menu page

--- coffee-house/src/js/files/script.js
+++ coffee-house/src/js/files/script.js
@@ -121,3 +121,50 @@ const initfavoriteSlider = () => {
 }
 
 initfavoriteSlider();
+
+//====================================================================
+
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass, display = 'flex') => {
+  const header = document.querySelector(headerSelector);
+
+  if (!header) return;
+
+  const tab = header.querySelectorAll(tabSelector);
+  const content = header.querySelectorAll(contentSelector);
+
+  const hideTabContent = () => {
+    content.forEach(item => {
+      item.style.display = 'none'
+    });
+    tab.forEach(item => {
+      item.classList.remove(activeClass)
+    });
+  };
+
+  const showTabContent = (i = 0) => {
+    content[i].style.display = display
+    tab[i].classList.add(activeClass)
+  };
+
+  hideTabContent()
+  showTabContent()
+
+  header.addEventListener('click', e => {
+    const target = e.target;
+    if (target.classList.contains(tabSelector.replace(/\./, '')) ||
+      target.parentNode.classList.contains(tabSelector.replace(/\./, ''))) {
+      tab.forEach((item, i) => {
+        if (target == item || target.parentNode == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  })
+};
+
+// ПЕРВЫЙ аргумент - класс всего нашего хедера табов.
+// ВТОРОЙ аргумент - класс конкретного элемента, при клике на который будет переключатся таб.
+// ТРЕТИЙ аргумент - класс того блока, который будет переключаться.
+// ЧЕТВЕРТЫЙ аргумент - класс активности, который будет добавлятся для таба, который сейчас активен.
+tabs('.menu-tabs', '.menu-tabs__header-item', '.menu-tabs__content-item', 'active');
