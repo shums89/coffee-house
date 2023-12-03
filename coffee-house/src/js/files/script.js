1c375164c9cbf0bc127e43c695215bfb5bbade32
Aleksandr Shumatbaev <sh889@ya.ru>
2023-12-03 11:03:06 +0500

    feat: add responsive for Menu page

--- coffee-house/src/js/files/script.js
+++ coffee-house/src/js/files/script.js
@@ -124,7 +124,7 @@ initfavoriteSlider();
 
 //====================================================================
 
-const tabs = (headerSelector, tabSelector, contentSelector, activeClass, display = 'flex') => {
+const tabs = (headerSelector, tabSelector, contentSelector, activeClassHeader, activeClassContent) => {
   const header = document.querySelector(headerSelector);
 
   if (!header) return;
@@ -132,18 +132,29 @@ const tabs = (headerSelector, tabSelector, contentSelector, activeClass, display
   const tab = header.querySelectorAll(tabSelector);
   const content = header.querySelectorAll(contentSelector);
 
+  const toggleButtonRefresh = (content) => {
+    const count = content.querySelectorAll('.products__item').length;
+    if (count > 4) {
+      document.querySelector('.menu-tabs__refresh').classList.remove('hidden');
+    }
+    else {
+      document.querySelector('.menu-tabs__refresh').classList.add('hidden');
+    }
+  };
+
   const hideTabContent = () => {
     content.forEach(item => {
-      item.style.display = 'none'
+      item.classList.remove(activeClassContent);
     });
     tab.forEach(item => {
-      item.classList.remove(activeClass)
+      item.classList.remove(activeClassHeader)
     });
   };
 
   const showTabContent = (i = 0) => {
-    content[i].style.display = display
-    tab[i].classList.add(activeClass)
+    content[i].classList.add(activeClassContent);
+    tab[i].classList.add(activeClassHeader)
+    toggleButtonRefresh(content[i]);
   };
 
   hideTabContent()
@@ -167,4 +178,5 @@ const tabs = (headerSelector, tabSelector, contentSelector, activeClass, display
 // ВТОРОЙ аргумент - класс конкретного элемента, при клике на который будет переключатся таб.
 // ТРЕТИЙ аргумент - класс того блока, который будет переключаться.
 // ЧЕТВЕРТЫЙ аргумент - класс активности, который будет добавлятся для таба, который сейчас активен.
-tabs('.menu-tabs', '.menu-tabs__header-item', '.menu-tabs__content-item', 'active');
+// ПЯТЫЙ аргумент - класс активности, который будет добавлятся для контента, который сейчас активен.
+tabs('.menu-tabs', '.menu-tabs__header-item', '.menu-tabs__content-item', 'active', 'active');
