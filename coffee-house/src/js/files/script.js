c4798f39ebe0ae10676058ba4b1b26e6771ab844
Aleksandr Shumatbaev <sh889@ya.ru>
2023-12-12 04:15:34 +0500

    fix: burger menu

--- coffee-house/src/js/files/script.js
+++ coffee-house/src/js/files/script.js
@@ -2,4 +2,10 @@ import { menuClose } from "./functions.js";
 
 const links = document.querySelectorAll('.menu__link');
 
-links.forEach(link => link.addEventListener('click', () => menuClose()));
+links.forEach(link => link.addEventListener('click', e => {
+  if (e.target.closest('.html-menu') && e.target.closest('.menu__link_menu')) {
+    e.preventDefault();
+  }
+
+  menuClose();
+}));
